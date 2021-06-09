af3cdb091b691cf285e3752115f65cc10d3f2387
Aakash <f20180887@pilani.bits-pilani.ac.in>
2021-06-10 00:36:02 +0530

    Config ffor firebase separated

--- src/services/authenticate.js
+++ src/services/authenticate.js
@@ -1,17 +1,9 @@
 import firebase from "firebase";
+import vars from "./config"
 require("firebase/auth");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAnShiAT2RseXUlnVUAf_JBSk0C37Zq_0E",
-  authDomain: "coding-club-competitive.firebaseapp.com",
-  projectId: "coding-club-competitive",
-  storageBucket: "coding-club-competitive.appspot.com",
-  messagingSenderId: "424350384226",
-  appId: "1:424350384226:web:7fb0fde3a2620e95f6513b",
-  measurementId: "G-9K9XP8WS3L",
-};
-
-const storageVar = "authUser";
+const storageVar = vars.authStorageVar;
+const firebaseConfig = vars.firebaseConfig;
 
 function getLoggedInUser() {
   const str = localStorage.getItem(storageVar);
@@ -62,7 +54,6 @@ const logOut = (result, error) => {
       },
       function (error) {
         console.error("Sign Out Error", error);
-        alert(error);
       }
     );
 };
