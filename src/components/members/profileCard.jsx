2bab0ba1da006faf8c83536511f49d5b50f23eda
Aakash <f20180887@pilani.bits-pilani.ac.in>
2021-06-10 16:18:47 +0530

    Year Replaced by ID

--- src/components/members/profileCard.jsx
+++ src/components/members/profileCard.jsx
@@ -13,14 +13,6 @@ const ProfileCard = (props) => {
         onInfoClick
     } = props;
 
-    const endYearString = (ID) =>{
-        // TODO: based on year and branch (dual or single), return the ending year
-        return "Present";
-    }
-
-    const JoinYear = BitsID.substring(0, 4);
-    const EndYear = endYearString(BitsID);
-
     // TODO: add component. If any of profile linnk is not available, don't show that icon
     return ( 
         <React.Fragment>
