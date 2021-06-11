0912f52a99a3d72727ef04e608ddf07291863b31
Aakash <f20180887@pilani.bits-pilani.ac.in>
2021-06-12 00:37:56 +0530

    Signout Bug Handled

--- src/pages/global.jsx
+++ src/pages/global.jsx
@@ -2,31 +2,47 @@ import React, { Component } from "react";
 import LoginPage from "./loginPage";
 import auth from "../services/authenticate";
 import HomePage from "./homepage";
+import { getMember } from "../services/database/members";
 
 class Global extends Component {
   state = {
     isWaiting: true,
     isLoggedIn: false,
+    userData: null,
   };
   isLoaded = false;
 
   componentDidMount() {
     auth.onAuthChange(async (usr) => {
+      let clientData = null;
+
+      if (usr && !usr.email.endsWith("pilani.bits-pilani.ac.in")) {
+        console.log("Bug handled!");
+        await auth.logOutAsync();
+        return;
+      }
+      if (usr !== null) {
+        try {
+          clientData = await getMember(usr.email);
+        } catch (ex) {}
+      }
+
       if (!this.isLoaded) {
         this.setState({
           isLoggedIn: usr !== null,
           isWaiting: false,
+          userData: clientData,
         });
         this.isLoaded = true;
       } else {
-        this.setState({ isLoggedIn: usr !== null });
+        this.setState({ isLoggedIn: usr !== null, userData: clientData });
       }
     });
   }
 
   onSignInClick = async () => {
     try {
-      await auth.clientLoginAsync();
+      this.client = await auth.clientLoginAsync();
       const idToken = await auth.userIdTokenAsync();
 
       console.log("Sign in completed, Sending response to server");
@@ -73,7 +89,7 @@ class Global extends Component {
                 this.setState({ isWaiting: false });
               }}
             />
-            <HomePage Content={null} />
+            <HomePage Content={this.state.userData} />
           </div>
         )}
       </React.Fragment>
