3189f39231dfd525f67447ad7f8d819e304b2375
kalashshah11 <kalashshah11@gmail.com>
2021-06-12 01:01:12 +0530

    Further Chenges for Profile Image

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import ReactJson from 'react-json-view';
-
+import ProfileImage from './images';
 class HomePage extends Component {
   
   render() {
+    var image = <ProfileImage Content={this.props.Content}/>;
     return this.props.Content && (<ReactJson src={this.props.Content}/>
     );
   }
