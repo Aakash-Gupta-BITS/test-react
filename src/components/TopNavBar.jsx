8a5ed9edbd5c853d93d2c966debfa9b160c60a12
Aakash <f20180887@pilani.bits-pilani.ac.in>
2021-06-23 07:13:15 +0530

    Responsive Navbar Implemented

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,29 +1,64 @@
 import React from "react";
-import { Flex, Spacer, Button, Heading, Center, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Spacer,
+  Button,
+  Heading,
+  Center,
+  Box,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const TopNavigationBar = ({ title, names, onClick }) => {
   return (
-    <Flex p={0} m={0} bg="gray.900" boxShadow="base">
-      <Center pl={4}>
-        <Heading size="lg">{title}</Heading>
-      </Center>
-      <Spacer />
-      <Box>
-        {names.map((currentelement, index) => (
-          <Button
-            key={index}
-            borderRadius={0}
-            bg="base"
-            height={16}
-            fontSize="lg"
-            onClick={() => onClick(currentelement)}
-            minWidth={24}
-          >
-            {currentelement}
-          </Button>
-        ))}
-      </Box>
-    </Flex>
+    <>
+      <Flex
+        p={0}
+        m={0}
+        bg="gray.900"
+        boxShadow="base"
+        flexDir={["column", "column", "row", "row"]}
+      >
+        <Center pl={4} h={[12,12,16,16]}>
+          <Heading size="lg">{title}</Heading>
+        </Center>
+        <Spacer />
+        <Center display={["flex", "flex", "none", "none"]}>
+          {names.map((currentelement, index) => (
+            <Button
+              key={index}
+              borderRadius={0}
+              bg="base"
+              fontSize="sm"
+              onClick={() => onClick(currentelement)}
+            >
+              {currentelement}
+            </Button>
+          ))}
+        </Center>
+        <Flex display={["none", "none", "flex", "flex"]}>
+          <Center>
+            {names.map((currentelement, index) => (
+              <Button
+                key={index}
+                borderRadius={0}
+                bg="base"
+                fontSize="lg"
+                h={16}
+                onClick={() => onClick(currentelement)}
+              >
+                {currentelement}
+              </Button>
+            ))}
+          </Center>
+        </Flex>
+      </Flex>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./routes/App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+
+const breakpoints = createBreakpoints({
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+})
 
 const theme = extendTheme({
   config: {
     useSystemColorMode: true,
     initialColorMode: "dark"
-  }
+  },
+  breakpoints
 });
 
 ReactDOM.render(
